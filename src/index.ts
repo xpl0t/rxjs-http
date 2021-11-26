13e4e9b996151e8990dde17b7c6371217dfe1eb7
Johannes Weih <johannes.weih@schueller.de>
2021-11-26 11:23:09 +0100

    Refactored index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,73 @@
-import 'whatwg-fetch';
-import { RequestMapper } from './mappers/request.mapper';
-import { Observable } from 'rxjs';
-import { fromPromise } from 'rxjs/internal-compatibility';
+import fetch from 'node-fetch';
+import { from, Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-import { HttpRequestConfig } from './types/http-request-config.class';
-import { IHttp } from './types/http.interface';
+import { HttpErrorHandler } from "./helpers/http-error-handler.helper";
+import { RequestMapper } from './mappers/request.mapper';
 import { HttpRequestConfigurations } from './types/http-configurations.enum';
 import { IHttpInterceptor } from './types/http-interceptor.interface';
 import { HttpInterceptors } from './types/http-interceptors.class';
+import { HttpRequestConfig } from './types/http-request-config.class';
 import { HttpRequest } from './types/http-request.class';
 import { HttpResponse } from './types/http-response.class';
-import {HttpErrorHandler} from "./helpers/http-error-handler.helper";
+import { IHttp } from './types/http.interface';
 
 export class RxJSHttpClient implements IHttp {
-    private readonly _requestInterceptors: HttpInterceptors<HttpRequest>;
-    private readonly _responseInterceptors: HttpInterceptors<HttpResponse>;
+    private readonly _reqInterceptors: HttpInterceptors<HttpRequest>;
+    private readonly _resInterceptors: HttpInterceptors<HttpResponse>;
 
-    constructor(requestInterceptors: Array<IHttpInterceptor<HttpRequest>> = [],
-                responseInterceptors: Array<IHttpInterceptor<HttpResponse>> = []) {
-        this._requestInterceptors = new HttpInterceptors<HttpRequest>(requestInterceptors);
-        this._responseInterceptors = new HttpInterceptors<HttpResponse>(responseInterceptors);
+    constructor(reqInterceptors: IHttpInterceptor<HttpRequest>[] = [],
+                resInterceptors: IHttpInterceptor<HttpResponse>[] = []) {
+        this._reqInterceptors = new HttpInterceptors<HttpRequest>(reqInterceptors);
+        this._resInterceptors = new HttpInterceptors<HttpResponse>(resInterceptors);
     }
 
     public get(url: string, config: Partial<HttpRequestConfig> = {}): Observable<HttpResponse> {
-        const request = this._requestInterceptors.execute(new HttpRequest(url, config));
+        const request = this._reqInterceptors.execute(new HttpRequest(url, config));
         const configObject: RequestInit = RequestMapper.for(request, HttpRequestConfigurations.GET);
 
-        return fromPromise(fetch(url, configObject))
-            .pipe(
-                map((response) => this._responseInterceptors.execute(new HttpResponse(response))),
-                tap(HttpErrorHandler.throwIfNotOkResponse)
-            );
+        return from(fetch(url, configObject as any)).pipe(
+            map(res => this._resInterceptors.execute(new HttpResponse(res))),
+            tap(HttpErrorHandler.throwIfNotOkResponse)
+        );
     }
 
     public post(url: string, config: Partial<HttpRequestConfig>): Observable<HttpResponse> {
-        const request = this._requestInterceptors.execute(new HttpRequest(url, config));
+        const request = this._reqInterceptors.execute(new HttpRequest(url, config));
         const configObject: RequestInit = RequestMapper.for(request, HttpRequestConfigurations.POST);
 
-        return fromPromise(fetch(url, configObject))
-            .pipe(
-                map((response) => this._responseInterceptors.execute(new HttpResponse(response))),
-                tap(HttpErrorHandler.throwIfNotOkResponse)
-            );
+        return from(fetch(url, configObject as any)).pipe(
+            map((response) => this._resInterceptors.execute(new HttpResponse(response))),
+            tap(HttpErrorHandler.throwIfNotOkResponse)
+        );
     }
 
     public put(url: string, config: Partial<HttpRequestConfig>): Observable<HttpResponse> {
-        const request = this._requestInterceptors.execute(new HttpRequest(url, config));
+        const request = this._reqInterceptors.execute(new HttpRequest(url, config));
         const configObject: RequestInit = RequestMapper.for(request, HttpRequestConfigurations.PUT);
 
-        return fromPromise(fetch(url, configObject))
-            .pipe(
-                map((response) => this._responseInterceptors.execute(new HttpResponse(response))),
-                tap(HttpErrorHandler.throwIfNotOkResponse)
-            );
+        return from(fetch(url, configObject as any)).pipe(
+            map((response) => this._resInterceptors.execute(new HttpResponse(response))),
+            tap(HttpErrorHandler.throwIfNotOkResponse)
+        );
     }
 
     public patch(url: string, config: Partial<HttpRequestConfig>): Observable<HttpResponse> {
-        const request = this._requestInterceptors.execute(new HttpRequest(url, config));
+        const request = this._reqInterceptors.execute(new HttpRequest(url, config));
         const configObject: RequestInit = RequestMapper.for(request, HttpRequestConfigurations.PATCH);
 
-        return fromPromise(fetch(url, configObject))
-            .pipe(
-                map((response) => this._responseInterceptors.execute(new HttpResponse(response))),
-                tap(HttpErrorHandler.throwIfNotOkResponse)
-            );
+        return from(fetch(url, configObject as any)).pipe(
+            map((response) => this._resInterceptors.execute(new HttpResponse(response))),
+            tap(HttpErrorHandler.throwIfNotOkResponse)
+        );
     }
 
     public delete(url: string, config: Partial<HttpRequestConfig>): Observable<HttpResponse> {
-        const request = this._requestInterceptors.execute(new HttpRequest(url, config));
+        const request = this._reqInterceptors.execute(new HttpRequest(url, config));
         const configObject: RequestInit = RequestMapper.for(request, HttpRequestConfigurations.DELETE);
 
-        return fromPromise(fetch(url, configObject))
-            .pipe(
-                map((response) => this._responseInterceptors.execute(new HttpResponse(response))),
-                tap(HttpErrorHandler.throwIfNotOkResponse)
-            );
+        return from(fetch(url, configObject as any)).pipe(
+            map((response) => this._resInterceptors.execute(new HttpResponse(response))),
+            tap(HttpErrorHandler.throwIfNotOkResponse)
+        );
     }
 }
